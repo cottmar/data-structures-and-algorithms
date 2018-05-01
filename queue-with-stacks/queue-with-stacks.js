78a191cddfd0171e4a118a3fa21ab35295321f2f
cara ottmar <cara.ottmar@gmail.com>
2018-04-30 21:10:07 -0700

    started js but can't complete

---
+++ queue-with-stacks/queue-with-stacks.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Queue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+// pushes values into Stack 1
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    
+  }
+}
