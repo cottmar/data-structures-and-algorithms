2605b8695d8d76ecff2ad5a5c08f6038ba2d33c0
cara ottmar <cara.ottmar@gmail.com>
2018-10-18 15:48:23 -0700

    worked on reversing ints, stings, and a hashmap problem

--- arrays/reverse-an-array/reverse-an-array.js
+++ arrays/reverse-an-array/reverse-an-array.js
@@ -1,9 +1,9 @@
-// function reverseArray(array) {
-//   let newArray = [];
-//   for (let i = array.length; i > 0; i--) {
-//     newArray.push(array.pop());
-//   }
-//     return newArray
-// }
+function reverseArray(array) {
+  let newArray = [];
+  for (let i = array.length; i > 0; i--) {
+    newArray.push(array.pop());
+  }
+  return newArray;
+}
 
-// console.log(reverseArray([1,2,3,4,5,6]));
+console.log(reverseArray([1, 2, 3, 4, 5, 6]));
