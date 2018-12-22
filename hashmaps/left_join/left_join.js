65aa05c56936e1f3c8b25b13a29b9a67386a5caf
cara ottmar <cara.ottmar@gmail.com>
2018-12-22 13:42:51 -0800

    added leftJoins and testing

--- hashmaps/left_join/left_join.js
+++ hashmaps/left_join/left_join.js
@@ -1,5 +1,21 @@
-still working on JS.
+'use strict';
 
-const leftJoin = (hashLeft, hashRight) => {
-  const map = {};
-}
+const codeLeftJoin = module.exports = {};
+
+codeLeftJoin.leftjoins = (hashmap1, hashmap2) => {
+  const returnObj = {};
+  const keyArr = Array.from(hashmap1.keys());
+
+  for (let i = 0; i < keyArr.length; i++) {
+    const newKey = keyArr[i];
+    const leftVal = hashmap1.get(newKey);
+    let rightVal = hashmap2.get(newKey);
+    
+    if (rightVal === undefined) {
+      rightVal = null;
+    }
+    returnObj[newKey] = [leftVal, rightVal];
+  }
+  return returnObj;
+};
+
